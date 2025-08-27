94a637be8b42b3cf2b072f1ffad055ba31aa8a00
André Nakano Miyake <andre.nmiyake@gmail.com>
2025-08-27 20:51:40 -0300

    Primeiro commit

--- exercicio1.m
+++ exercicio1.m
@@ -7,8 +7,26 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+t = zeros(20, 1);
+t(1) = x0;
+erro = zeros(imax, 1);
+
+for ii = 1:imax %usar ii e n i pq i eh imaginarium
+  if ii ~= 1
+    erro(ii) = abs(t(ii) - t(ii-1))/t(ii); %abs é modulo
+    if erro(ii) < es
+      break
+    endif
+  endif
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii)); %; faz sumir
+endfor
+  last_index = find(t, 1, 'last');
+  resultado = t(last_index)
+
+  erro = erro(ii)
+
+  resultado_interacoes = ii
+t = resultado; % alterar
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
