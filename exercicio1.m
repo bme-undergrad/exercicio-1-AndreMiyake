be3c21c055b35b7fb34acc97f97894239fd12cca
André Nakano Miyake <andre.nmiyake@gmail.com>
2025-09-02 22:20:07 -0300

    Ajustes finais

--- exercicio1.m
+++ exercicio1.m
@@ -6,22 +6,20 @@ imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-
 t = zeros(imax, 1);
 t(1) = x0;
-erro = zeros(imax+1, 1);
+erro = zeros(imax, 1);
 
-for ii = 1:length(t) %usar ii e n i pq i eh imaginarium
+for ii = 1:length(t)-1 %usar ii e n i pq i eh imaginarium
   if ii ~= 1
     erro(ii) = abs(t(ii) - t(ii-1))/t(ii); %abs é modulo
     if erro(ii) < es
       break
     endif
   endif
-  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii)); %';' faz sumir
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii)); %; faz sumir
 endfor
-
-t(ii)
+t = t(ii); % alterar
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
